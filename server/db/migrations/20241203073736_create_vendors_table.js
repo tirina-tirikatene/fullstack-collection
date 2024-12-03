f4ea7f0b30fd572e4b1492ae7a56724c7997d5ca
Tirina Tirikatene <tirinatirikatene@yahoo.com>
2024-12-03 22:45:50 +1300

    Created vendors table

---
+++ server/db/migrations/20241203073736_create_vendors_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('vendors', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('location')
+    table.string('description')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('vendors')
+}
+
